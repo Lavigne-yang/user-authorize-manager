a5defbbee6a2f3371e4516be1a959d641a7cf657
yangyongqi <yongqi.yang@bessystem.com>
2023-08-29 15:13:53 +0800

    #UAM-0004 * 依赖管理

---
+++ user-authorize-server/src/main/java/com/yyq/sso/authorize/server/UserAuthorizeApplication.java
@@ -0,0 +1,13 @@
+package com.yyq.sso.authorize.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthorizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthorizeApplication.class);
+    }
+}
---
+++ user-authorize-server/src/main/java/com/yyq/sso/authorize/server/config/AuthorizationServerConfig.java
@@ -0,0 +1,9 @@
+package com.yyq.sso.authorize.server.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+
+
+}
